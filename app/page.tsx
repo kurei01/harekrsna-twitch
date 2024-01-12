9b0c23900a82d6375a5085488a0b14a2db6a3041
anh.nt <anh.nt@vn-cubesystem.com>
2024-01-12 16:16:28 +0700

    global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Page() {
-  return <div>Authenticated user can see this</div>;
+  return <div className="flex flex-col gap-y-4">
+    <h1>Dashboard</h1>
+    <UserButton afterSignOutUrl="/"/>
+  </div>
 }
