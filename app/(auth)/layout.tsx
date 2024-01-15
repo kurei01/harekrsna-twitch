41b209a52c409bfa08e1b3898768d6b1fc691eda
anh.nt <anh.nt@vn-cubesystem.com>
2024-01-15 16:56:55 +0700

    add webhook route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
+import { Logo } from "./_components/logo";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex flex-col justify-center items-center">
+    <div className="h-full flex flex-col justify-center items-center space-y-4">
+      <Logo />
       {children}
     </div>
   );
