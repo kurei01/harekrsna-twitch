e13b165f2022a1f5f9f1c308579a86fa9c395883
anh.nt <anh.nt@vn-cubesystem.com>
2024-01-17 10:53:52 +0700

    collapsed and expand

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
+
+export const Sidebar = () => {
+  return (
+    <div>
+      <Wrapper>
+        <Toggle />
+      </Wrapper>
+    </div>
+  );
+};
