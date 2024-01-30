a77b97f9511cca48afde1bc5d3bbd7a3d9e7cb29
anh.nt <anh.nt@vn-cubesystem.com>
2024-01-30 18:23:41 +0700

    navigation dashboard

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -4,6 +4,8 @@ import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
 
+import { NavItem, NavItemSkeleton } from "./nav-item";
+
 export const Navigation = () => {
   const pathname = usePathname();
   const { user } = useUser();
@@ -31,10 +33,26 @@ export const Navigation = () => {
     },
   ];
 
+  if (!user?.username) {
+    return (
+      <ul className="space-y-2">
+        {[...Array(4)].map((_, i) => (
+          <NavItemSkeleton key={i} />
+        ))}
+      </ul>
+    );
+  }
+
   return (
-    <ul>
+    <ul className="space-y-2 px-2 pt-4 lg:pt-0">
       {routes.map((route) => (
-        <div key={route.href}>{route.label}</div>
+        <NavItem
+          key={route.href}
+          label={route.label}
+          icon={route.icon}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
       ))}
     </ul>
   );
