6a46629709ba4a57912e20563f83360fce42c359
anh.nt <anh.nt@vn-cubesystem.com>
2024-02-20 21:01:49 +0700

    Chat component

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { ConnectionState } from "livekit-client";
+import { useMediaQuery } from "usehooks-ts";
+import {
+  useChat,
+  useConnectionState,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+
+import { useChatSideBar } from "@/store/use-chat-sidebar";
+
+interface ChatProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerName: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  hostName,
+  hostIdentity,
+  viewerName,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { variant, onExpand } = useChatSideBar((state) => state);
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+
+  useEffect(() => {
+    if (matches) {
+      onExpand();
+    }
+  }, [matches, onExpand]);
+
+  const reversedMessages = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) {
+      return;
+    }
+
+    send(value);
+    setValue("");
+  };
+
+  const onChange = (value: string) => {
+    setValue(value);
+  };
+
+  return (
+    <div className="flex flex-col bg-background border-l border-b pt-0 h-[calc(100vh-80px)]">
+      Chats
+    </div>
+  );
+};
